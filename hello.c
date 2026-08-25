c9a664b750cb81852a0faefb10f91f78b6ddf63a
rkuouvup <rkuouvup@gmail.com>
2026-08-25 09:57:44 -0600

    Aug 25 in-class exercise

---
+++ hello.c
@@ -0,0 +1,7 @@
+// Directives
+#include <stdio.h>
+
+int main() {
+    printf("Hello World\n");
+    return 0;
+}
---
+++ hello2.c
@@ -0,0 +1,21 @@
+/*
+    hello2.c
+    Example of C program general form
+    Author: 
+    Date: 
+*/
+
+// Directives
+#include <stdio.h>
+
+// return_type func_name(arguments) {}
+int main() {
+    // static type programming languaged
+    // data_typa var_name;
+    int ver;
+    ver = 23.5; 
+    printf("Hello, C%f\n", ver); 
+    int height, length, width = 8;
+    printf("h: %d, l: %d, w: %d\n", height, length, width);
+    return 0;
+}
